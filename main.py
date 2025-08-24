fba6c612de1079b4cedea908450b5e96e67cbbcb
joonmo-you <joonmo.you@gmail.com>
2025-08-24 17:30:57 +0900

    Collisions

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 import pygame
 from player import Player
 from asteroid import Asteroid
@@ -29,6 +30,11 @@ def main():
                 return
 
         updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
 
         screen.fill("black")
         
